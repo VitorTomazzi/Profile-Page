d016d79f4bd66ef911679c329f7ff32264730d33
Vitor Tomazzi <vitortomazzi@Vitors-MacBook-Pro.local>
2019-08-26 12:18:38 -0400

    adding to GitHub

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// do an axios get to see how many people are visiting the website 
+
+// do an axios get request to list github Repos 
+axios.get(`https://api.github.com/users/VitorTomazzi/repos?per_page=100`)
+.then((result) => {
+    
+})
+.catch((err) => {
+    
+});
